171f91a99c6db20a6a2029f36227a4c38af4e52d
denis <denis.tumel.98@mail.ru>
2019-05-12 19:54:38 +0300

    add db connect

--- src/main/java/com/exemple/hair/database/DB.java
+++ src/main/java/com/exemple/hair/database/DB.java
@@ -3,5 +3,5 @@ package com.exemple.hair.database;
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/postgres?currentSchema=hair_salon", "postgres", "root");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/postgres?currentSchema=hair_salon", "null", "root");
 }
